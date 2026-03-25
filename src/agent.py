a802f8a0f005f4fe64c2e264ac25e7b0b36e4b03
li343512 <zj827136091@gmail.com>
2026-03-25 19:17:04 +0800

    Implement DQN Agent in src/agent.py

---
+++ src/agent.py
@@ -0,0 +1,57 @@
+import torch
+import torch.nn as nn
+import numpy as np
+from collections import deque
+import random
+
+class DQNAgent:
+    def __init__(self, state_size, action_size):
+        self.state_size = state_size
+        self.action_size = action_size
+        self.memory = deque(maxlen=2000)
+        self.gamma = 0.95  # discount rate
+        self.epsilon = 1.0  # exploration rate
+        self.epsilon_decay = 0.995
+        self.epsilon_min = 0.01
+        self.model = self._build_model()  # Neural Network
+
+    def _build_model(self):
+        # Neural Net for Deep-Q learning Model
+        model = nn.Sequential(
+            nn.Linear(self.state_size, 24),
+            nn.ReLU(),
+            nn.Linear(24, 24),
+            nn.ReLU(),
+            nn.Linear(24, self.action_size)
+        )
+        model.compile(loss='mse', optimizer='adam')
+        return model
+
+    def remember(self, state, action, reward, next_state, done):
+        self.memory.append((state, action, reward, next_state, done))
+
+    def act(self, state):
+        if np.random.rand() <= self.epsilon:
+            return random.randrange(self.action_size)
+        act_values = self.model.predict(state)
+        return np.argmax(act_values[0])  # returns the value of the action
+
+    def replay(self, batch_size):
+        minibatch = random.sample(self.memory, batch_size)
+        for state, action, reward, next_state, done in minibatch:
+            target = reward
+            if not done:
+                target += self.gamma * np.amax(self.model.predict(next_state)[0])
+            target_f = self.model.predict(state)
+            target_f[0][action] = target
+            self.model.fit(state, target_f, epochs=1, verbose=0)
+
+    def decay_epsilon(self):
+        if self.epsilon > self.epsilon_min:
+            self.epsilon *= self.epsilon_decay
+
+    def save(self, name):
+        self.model.save(name)
+
+    def load(self, name):
+        self.model = load_model(name)
